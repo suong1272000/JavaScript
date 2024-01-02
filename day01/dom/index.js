136cc7b0f05d0705182b8a74550a0d520f6fa890
kseonghee <kseonghee0127@gmail.com>
2024-01-02 15:34:48 +0900

    JavaScript 정리

---
+++ day01/dom/index.js
@@ -0,0 +1,6 @@
+//1. dom을 이용하기
+const a = document.querySelector('.box')
+//querySelector는 어떤것을 찾아주세요 라는 의미
+//.box 박스를 찾고 =(넣어주세요) a라는 공간에
+a.style.backgroundColor = 'skyblue'
+a.style.fontSize = "30px"
