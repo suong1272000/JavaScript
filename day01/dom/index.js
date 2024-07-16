2ee0b846a6bfd4e55c00e2725cab23b721070dd4
suong1272000 <66342329+suong1272000@users.noreply.github.com>
2024-07-16 09:57:49 +0900

    feat: prettier test

--- day01/dom/index.js
+++ day01/dom/index.js
@@ -3,9 +3,9 @@
 //3. querySelector('찾기 대상')
 //4. const 별명 (임시 보관) / .box 박스를 찾고 =(넣어주세요) box라는 공간에
 //5. 찾은 대상의 css 바꾸기 (별명.style.속성)
-const box = document.querySelector('.box')
-box.style.backgroundColor = 'skyblue'
-box.style.fontSize = "30px"
+const box = document.querySelector(".box");
+box.style.backgroundColor = "skyblue";
+box.style.fontSize = "30px";
 //6. 찾은 대상의 contents 바꾸기
 // box.innerText = '새해는 한강으로!' / 얘는 글씨만 가능
-box.innerHTML = '<div class="mini">새해는 한강뷰로!</div>' // 얘는 이런 유형도 가능
+box.innerHTML = '<div class="mini">새해는 한강뷰로!</div>'; // 얘는 이런 유형도 가능
